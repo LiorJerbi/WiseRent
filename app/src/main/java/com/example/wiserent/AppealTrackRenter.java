0e5cb6079eb20ca62b7b1aea6d2b917b9fe23cfd
LiorJerbi <lior.Jerbi@msmail.Ariel.ac.il>
2024-01-21 23:26:28 +0200

    added few more screens

---
+++ app/src/main/java/com/example/wiserent/AppealTrackRenter.java
@@ -0,0 +1,14 @@
+package com.example.wiserent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppealTrackRenter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appeal_track_renter);
+    }
+}
---
+++ app/src/main/java/com/example/wiserent/NewAppealRented.java
@@ -0,0 +1,14 @@
+package com.example.wiserent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAppealRented extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_appeal_rented);
+    }
+}
